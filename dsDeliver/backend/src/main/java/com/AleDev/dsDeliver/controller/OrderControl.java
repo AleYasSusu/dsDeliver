54a7f945571e94d554acaf57db11f2edbea88378
Alessandroyasmim <Alessandro.souza@edu.pucrs.br>
2021-08-29 10:00:33 -0300

    implementado metodos para inserir pedidos

--- dsDeliver/backend/src/main/java/com/AleDev/dsDeliver/controller/OrderControl.java
+++ dsDeliver/backend/src/main/java/com/AleDev/dsDeliver/controller/OrderControl.java
@@ -1,12 +1,16 @@
 package com.AleDev.dsDeliver.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.AleDev.dsDeliver.dto.OrderDto;
 import com.AleDev.dsDeliver.services.OrderService;
@@ -16,13 +20,20 @@ import com.AleDev.dsDeliver.services.OrderService;
 public class OrderControl {
 	@Autowired
 	private OrderService orderService;
-	
-	
-	
+
 	@GetMapping
-	public ResponseEntity<List<OrderDto>> findAll(){
-	List<OrderDto> list = orderService.findAll();
-	return ResponseEntity.ok().body(list);
+	public ResponseEntity<List<OrderDto>> findAll() {
+		List<OrderDto> list = orderService.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@PostMapping
+	public ResponseEntity<OrderDto> insert(@RequestBody OrderDto dto) {
+		dto = orderService.insert(dto);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+
+		return ResponseEntity.created(uri).body(dto);
+
 	}
 }
-
--- dsDeliver/backend/src/main/java/com/AleDev/dsDeliver/services/OrderService.java
+++ dsDeliver/backend/src/main/java/com/AleDev/dsDeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.AleDev.dsDeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,14 +9,20 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.AleDev.dsDeliver.dto.OrderDto;
+import com.AleDev.dsDeliver.dto.ProductDto;
 import com.AleDev.dsDeliver.entities.Order;
+import com.AleDev.dsDeliver.entities.OrderStatus;
+import com.AleDev.dsDeliver.entities.Product;
 import com.AleDev.dsDeliver.repositories.OrderRepository;
+import com.AleDev.dsDeliver.repositories.ProductRepository;
 @Service
 public class OrderService {
 
 
 	@Autowired
 	private OrderRepository orderRepository;
+	@Autowired
+	private ProductRepository  productRepository;
 
 	@Transactional(readOnly = true)
 	public List<OrderDto> findAll() {
@@ -23,5 +30,16 @@ public class OrderService {
 		return list.stream().map(x -> new OrderDto(x)).collect(Collectors.toList());
 	}
 
+	@Transactional
+	public OrderDto insert(OrderDto dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
+		for(ProductDto p : dto.getProducts()) {
+			Product products = productRepository.getOne(p.getId());
+			order.getProducts().add(products);
+		}
+		order = orderRepository.save(order);
+		return new OrderDto(order);
+	}
+	
 }
 
